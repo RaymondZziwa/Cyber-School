171339f1d36e6277732c5d39761f870e8309627d
Zziwa Raymond Ian <raymondzian@gmail.com>
2021-12-09 23:24:04 +0300

    updated 11 folder

---
+++ 11/scripts/Qn5.js
@@ -0,0 +1,22 @@
+
+let rightAns = document.querySelector('.review');
+let wrongAns = document.querySelector('.Tryagain');
+let next = document.querySelector('.proceed');
+let op1 = document.getElementById('option1');
+let op2 = document.getElementById('option2');
+
+const correct = () => {
+    rightAns.style.visibility = 'visible';
+    op1.style.backgroundImage = 'linear-gradient(#61686b,#61686b)';
+    op2.style.visibility = 'hidden';
+    wrongAns.style.visibility = 'hidden';
+    next.style.visibility='visible';
+}
+
+const wrong = () => {
+  wrongAns.style.visibility = 'visible';
+}
+
+const forward = () => {
+  location.href="Qn6.html";
+}
